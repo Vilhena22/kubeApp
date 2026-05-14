b8c177b7cf6dd4e1067dc13581e384685d47c42e
PedroGomesReis <2232816@my.ipleiria.pt>
2026-05-14 20:49:01 +0100

    namespace service done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
+import Handlers.AppSetup;
 import api.KubernetesClient;
-import io.kubernetes.client.openapi.models.V1Node;
-import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.*;
+import service.DeploymentService;
 
 import javax.swing.*;
 import java.util.List;
@@ -14,13 +15,13 @@ public class Main {
                     AppSetup.getK3sToken()
             );
 
-            List<V1Pod> pods = client.getPodService().listPods("default");
+//            List<V1Pod> pods = client.getPodService().listPods("default");
+//
+//            pods.forEach(pod ->
+//                    System.out.println("Pod: " + Objects.requireNonNull(pod.getMetadata()).getName())
+//            );
 
-            pods.forEach(pod ->
-                    System.out.println("Pod: " + Objects.requireNonNull(pod.getMetadata()).getName())
-            );
-
-            System.out.println(client.getNodeService().getNode("tl2node1"));
+            //System.out.println(client.getNodeService().getNode("tl2node1"));
 
 
             /*List<V1Node> nodes = client.getNodeService().listNodes();
@@ -28,6 +29,19 @@ public class Main {
             nodes.forEach(node ->
                     System.out.println("Node: " + Objects.requireNonNull(node.getMetadata()).getName())
             );*/
+
+//            V1DeploymentList deps = client.getDeploymentService().getAllDeployments();
+//
+//            for (V1Deployment dep : deps.getItems()){
+//                System.out.println(dep.getMetadata().getName());
+//            }
+
+            V1NamespaceList nm = client.getNamespaceService().getAllNamespaces();
+
+            for (V1Namespace namespace : nm.getItems()){
+                V1ObjectMeta metadata = namespace.getMetadata();
+                System.out.println(metadata.getName());
+            }
 
         } catch (Exception e) {
             System.out.println("Error Connecting: " + e.getMessage());
--- src/main/java/service/NamespaceService.java
+++ src/main/java/service/NamespaceService.java
@@ -1,4 +1,28 @@
 package service;
 
-public class NamespaceService {
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Namespace;
+import io.kubernetes.client.openapi.models.V1NamespaceList;
+import io.kubernetes.client.openapi.models.V1Status;
+
+public class NamespaceService extends V1Namespace {
+
+    private CoreV1Api nm = new CoreV1Api();
+
+    public V1Namespace getNamespace(String namespace) throws ApiException {
+        return nm.readNamespace(namespace).execute();
+    }
+
+    public V1NamespaceList getAllNamespaces() throws ApiException {
+        return nm.listNamespace().execute();
+    }
+
+    public void createNamespace(V1Namespace nome) throws ApiException {
+        nm.createNamespace(nome).execute();
+    }
+
+    public V1Status deleteNamespace(String namespace) throws ApiException {
+        return nm.deleteNamespace(namespace).execute();
+    }
 }
